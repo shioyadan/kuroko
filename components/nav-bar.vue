e322bdeea4426119bd3b63bbe8aaf1d5920a83ce
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2020-05-28 04:07:22 +0900

    Fix a PDF file name

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -72,7 +72,7 @@ module.exports = {
                     filters: [
                         {
                             name: "PDF",
-                            extensions: ["PDF"]
+                            extensions: ["pdf"]
                         }
                     ],
                     defaultPath: defaultFileName
